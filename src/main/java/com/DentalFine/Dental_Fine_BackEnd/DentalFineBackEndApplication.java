461fc4e5f96eb65b1567ceb51c905150b2158a57
Haziel Zavala <kevlinlal@gmail.com>
2026-02-26 15:20:56 -0600

    Carga de arquitectura inicial de Spring

---
+++ src/main/java/com/DentalFine/Dental_Fine_BackEnd/DentalFineBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.DentalFine.Dental_Fine_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalFineBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalFineBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DentalFine/Dental_Fine_BackEnd/DentalFineBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DentalFine.Dental_Fine_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalFineBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
